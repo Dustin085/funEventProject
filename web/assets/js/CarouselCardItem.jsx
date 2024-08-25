ba897a83520d60dcef60185e5813fe5a0202e3f4
Dustin085 <dustin830927@gmail.com>
2024-08-25 10:48:19 +0800

    index-eventList-json-finish

--- web/assets/js/CarouselCardItem.jsx
+++ web/assets/js/CarouselCardItem.jsx
@@ -1,7 +1,7 @@
 function CarouselCardItem() {
     return <>
         <a href="./event/event.html" className="carousel-card">
-            <figure className="fig"><img src="./assets/images/demo-event-pic/demo-event-pic-05.jpg" alt="" /></figure>
+            <figure className="fig"><img src="./assets/images/demo-event-pic/demo-event-pic-13.jpg" alt="" /></figure>
             <h3 className="title">【活動講座】繪本辦桌秀一下!親子活動-台中國家歌劇院</h3>
         </a>
     </>
