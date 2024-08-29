ea937a84a41acab87511940d0bb04a4f408e92aa
Dustin085 <dustin830927@gmail.com>
2024-08-29 17:46:44 +0800

    home-page-bot-caro-json

--- web/assets/js/CarouselCardItem.jsx
+++ web/assets/js/CarouselCardItem.jsx
@@ -1,4 +1,6 @@
-function CarouselCardItem({title="【活動講座】繪本辦桌秀一下!親子活動-台中國家歌劇院", imgUrl="./assets/images/demo-event-pic/demo-event-pic-13.jpg"}) {
+function CarouselCardItem({
+    title = "【活動講座】繪本辦桌秀一下!親子活動-台中國家歌劇院",
+    imgUrl = "./assets/images/demo-event-pic/for-bot-caro-pic1.jpg" }) {
     return <>
         <a href="./event/event.html" className="carousel-card">
             <figure className="fig"><img src={imgUrl} alt="" /></figure>
