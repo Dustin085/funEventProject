92233ba3d464f9068982b4f30554f11bf069c473
Dustin085 <dustin830927@gmail.com>
2024-09-01 16:48:57 +0800

    0901

---
+++ web/assets/js/MemberCenterSidebar.jsx
@@ -0,0 +1,80 @@
+function MemberCenterSidebar() {
+    return <>
+        <nav className="member-center-sidebar">
+            <div className="member-center-sidebar__intro">
+                <div className="member-avatar-box">
+                    <figure className="member-center-sidebar__avatar">
+                        <img src="./assets/images/member-default-avatar.png" alt="" />
+                    </figure>
+                    <button type="button" className="edit-member-avatar-btn"><object
+                        data="../assets/images/edit-avatar-icon.svg" type="image/svg+xml"></object></button>
+                </div>
+                <h3 className="member-center-sidebar__name">會員名字</h3>
+                <a href="#" className="member-center-sidebar__edit-link">管理個人資料</a>
+            </div>
+            <div className="member-center-sidebar__menu">
+                <div className="member-center-sidebar__list-box">
+                    <h4>我的活動</h4>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/my-ticket.svg)" }}
+                        ></div>
+                        <a href="#">我的票卷</a>
+                    </div>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/fun-point.svg)" }}
+                        ></div>
+                        <a href="#">Fun點數</a>
+                    </div>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/fun-coupon.svg)" }}
+                        ></div>
+                        <a href="#">Fun折價卷</a>
+                    </div>
+                </div>
+                <div className="split-line-row"></div>
+                <div className="member-center-sidebar__list-box">
+                    <h4>帳號設定</h4>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/account-manage.svg)" }}
+                        ></div>
+                        <a href="#">帳號管理</a>
+                    </div>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/my-comment.svg)" }}
+                        ></div>
+                        <a href="#">我的評論</a>
+                    </div>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/my-fav.svg)" }}
+                        ></div>
+                        <a href="#">我的蒐藏</a>
+                    </div>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/messenge-manage.svg)" }}
+                        ></div>
+                        <a href="#">訊息管理</a>
+                    </div>
+                </div>
+                <div className="split-line-row"></div>
+                <div className="member-center-sidebar__list-box">
+                    <h4>主辦中心</h4>
+                    <div className="member-center-sidebar__link-box">
+                        <div className="icon--small"
+                            style={{ backgroundImage: "url(./assets/images/member-center-sidebar-icon/account-switch.svg)" }}
+                        ></div>
+                        <a href="#">帳號切換</a>
+                    </div>
+                </div>
+
+                <button className="btn-logout--large funevent-btn" type="button">登出</button>
+            </div>
+        </nav>
+    </>
+}
