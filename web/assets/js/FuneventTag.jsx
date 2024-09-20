f9b56f5f21fa371b7d1580bee98081c4aede92da
Dustin085 <dustin830927@gmail.com>
2024-09-20 17:46:31 +0800

    0920

--- web/assets/js/FuneventTag.jsx
+++ web/assets/js/FuneventTag.jsx
@@ -4,19 +4,19 @@ function FuneventTag(props) {
     } = props;
 
     // 按下tag把data-tagName取出來放到localStorage的userSearchText，之後進入搜尋頁
-    const tagOnClickHandler = (ev) => {
-        ev.preventDefault();
-        let tagName = ev.target.dataset.tagName;
-        tagName = tagName.replace("#", "");
-        // 挑轉到搜尋頁
-        location.href = "./search.html?" + "search_query=" + tagName;
-    };
+    // const tagOnClickHandler = (ev) => {
+    //     ev.preventDefault();
+    //     let tagName = ev.target.dataset.tagName;
+    //     tagName = tagName.replace("#", "");
+    //     // 挑轉到搜尋頁
+    //     location.href = "./search.html?" + "search_query=" + tagName;
+    // };
 
     return <>
-        <a href="#"
+        <a href={"./search.html?" + "search_query=" + tagText.replace("#", "")}
             className="tag link"
             data-tag-name={tagText}
-            onClick={tagOnClickHandler}
+            // onClick={tagOnClickHandler}
         >{tagText}</a>
     </>
 }
